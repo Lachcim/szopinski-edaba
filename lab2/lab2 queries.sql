7b8a38623227724c605b4bace4445136406cef89
Michał Szopiński <mszopinski@mssnt.pl>
2022-01-07 01:34:32 +0100

    Add most commonly visited rooms

--- lab2/lab2 queries.sql
+++ lab2/lab2 queries.sql
@@ -1,3 +1,4 @@
+-- Track employee location over time
 SELECT
     Employees.GivenName || ' ' || Employees.Surname AS "Employee",
     TO_CHAR(ActionDate, 'YYYY-MM-DD HH24:MI:SS') AS "Action date",
@@ -10,3 +11,17 @@ JOIN Gates ON Gates.GateID = Actions.Gate
 JOIN Rooms DestRooms ON DestRooms.RoomId = COALESCE(NULLIF(Gates.RoomA, Actions.Room), NULLIF(Gates.RoomB, Actions.Room))
 WHERE ActionType = 1
 ORDER BY Employee, ActionDate;
+
+-- Find most commonly visited rooms
+SELECT
+    Destination,
+    COUNT(Destination) AS "Visit count"
+FROM (
+    SELECT
+        DestRooms.RoomNumber || ' (' || DestRooms.Description || ')' AS Destination
+    FROM Actions
+    JOIN Gates ON Gates.GateID = Actions.Gate
+    JOIN Rooms DestRooms ON DestRooms.RoomId = COALESCE(NULLIF(Gates.RoomA, Actions.Room), NULLIF(Gates.RoomB, Actions.Room))
+)
+GROUP BY Destination
+ORDER BY "Visit count" DESC;
