3e8247c835bf828a58543665bf6cd327bd04c7a4
Michał Szopiński <mszopinski@mssnt.pl>
2022-01-07 14:22:47 +0100

    Add floor 9 employees query

--- lab2/lab2 queries.sql
+++ lab2/lab2 queries.sql
@@ -28,8 +28,26 @@ ORDER BY "Visit count" DESC;
 
 -- Generate a table of rooms and their neighbors
 SELECT
-    Rooms.RoomNumber,
+    Rooms.RoomNumber as "Room number",
     Neighbors.RoomNumber AS Neighbor
 FROM Rooms
 LEFT JOIN Gates ON Gates.RoomA = RoomID OR Gates.RoomB = RoomID
-JOIN Rooms Neighbors ON (Neighbors.RoomID = Gates.RoomA OR Neighbors.RoomID = Gates.RoomB) AND Neighbors.RoomID <> Rooms.RoomID
+JOIN Rooms Neighbors ON (Neighbors.RoomID = Gates.RoomA OR Neighbors.RoomID = Gates.RoomB) AND Neighbors.RoomID <> Rooms.RoomID;
+
+-- View all employees permitted to enter rooms on floor 9 (except for the corridor)
+SELECT DISTINCT
+    Employees.GivenName || ' ' || Employees.Surname || ' (' || Employees.EmployeeID || ')' AS "Employee",
+    FloorPermissions.Expires
+FROM Employees
+JOIN (
+    SELECT
+        Permissions.Employee,
+        Permissions.PermissionID,
+        Permissions.Expires,
+        Rooms.RoomNumber
+    FROM Permissions
+    JOIN Rooms ON Rooms.RoomId = Permissions.Room
+) FloorPermissions ON
+    FloorPermissions.Employee = Employees.EmployeeID
+    AND FloorPermissions.RoomNumber LIKE '9___'
+    AND FloorPermissions.RoomNumber <> '9000';
